1efc55837870e0a345138b6db7ce1462002804a8
kaicikoglu <kaicikoglu@outlook.de>
2024-05-30 15:58:41 +0200

    fix navigation

--- lib/forum/screens/forum_screen.dart
+++ lib/forum/screens/forum_screen.dart
@@ -1,15 +1,19 @@
+import 'package:event_flow/widgets/base_screen.dart';
 import 'package:flutter/material.dart';
 
-class ForumScreen extends StatelessWidget {
+class ForumScreen extends StatefulWidget {
   const ForumScreen({super.key});
 
   @override
+  _ForumScreenState createState() => _ForumScreenState();
+}
+
+class _ForumScreenState extends State<ForumScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Forum'),
-      ),
-      body: const Center(
+    return const BaseScreen(
+      selectedIndex: 0,
+      child: Center(
         child: Text('Forum Screen'),
       ),
     );
