9c8a48d099059ef4c1e1e098ff43b58acbcf2ce1
kaicikoglu <kaicikoglu@outlook.de>
2024-06-26 12:39:28 +0200

    outsource business logic

--- lib/features/forum/screens/forum_screen.dart
+++ lib/features/forum/screens/forum_screen.dart
@@ -1,48 +1,27 @@
 import 'package:event_flow/features/forum/widgets/create_forum_topic.dart';
 import 'package:event_flow/widgets/base_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
 import '../../../data_models/event_data_model.dart';
-import '../../../data_models/forum_topic_data_model.dart';
-import '../../../services/isar_service.dart';
 import '../../../widgets/floating_action_button.dart';
 import '../../../widgets/wide_button.dart';
+import '../services/forum_controller.dart';
 
-class ForumScreen extends StatefulWidget {
+class ForumScreen extends ConsumerWidget {
   final Event event;
 
   const ForumScreen({super.key, required this.event});
 
   @override
-  _ForumScreenState createState() => _ForumScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final topics = ref.watch(forumControllerProvider(event));
+    final forumController = ref.read(forumControllerProvider(event).notifier);
 
-class _ForumScreenState extends State<ForumScreen> {
-  List<ForumTopic> topics = [];
+    void addTopic(String topicTitle) async {
+      await forumController.addTopic(topicTitle);
+    }
 
-  @override
-  void initState() {
-    super.initState();
-    loadTopics();
-  }
-
-  Future<void> loadTopics() async {
-    await IsarService().initializeIsar();
-    await widget.event.forumTopics.load();
-    setState(() {
-      topics = widget.event.forumTopics.toList();
-    });
-  }
-
-  void addTopic(String topicTitle) async {
-    final isar = IsarService().getIsar();
-    await widget.event.createForumTopic(isar, topicTitle);
-    await loadTopics();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return BaseScreen(
       backButton: BackButton(
         onPressed: () {
@@ -65,11 +44,10 @@ class _ForumScreenState extends State<ForumScreen> {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0),
-            // Adjust the gap as needed
             child: CustomWideButton(
               text: topics[index].title,
               onPressed: () {
-                context.push('/topic/${topics[index].id}');
+                context.push('/forumTopic', extra: topics[index]);
               },
             ),
           );
